7b800bfc469b1b54e48a9c6c957cc0e04547db17
OhJeong-Taek <ojt0128@gmail.com>
2023-12-01 10:00:24 +0900

    Time: 38 ms (81.61%) | Memory: 16.1 MB (76.25%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = set()
+        while True:
+            temp = 0
+            for x in str(n):
+                temp += int(x)**2
+            n = temp
+            if n == 1:
+                return True
+            if n in s:
+                return False
+            
+            s.add(n)
+
+    #4 16 37 58 89 125 30 9 81 65 61 37
