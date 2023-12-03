dd3dc035815d993547917c916b471644d12c648b
OhJeong-Taek <ojt0128@gmail.com>
2023-12-03 09:09:25 +0900

    Time: 38 ms (51.98%) | Memory: 16.3 MB (50.20%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -2,17 +2,23 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums: return []
         #to handle last element
-        nums.append(nums[-1]+2)
+        #nums.append(nums[-1]+2)
         
         ans = []
         start = nums[0]
         
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1] + 1:
-                if start != nums[i-1]: ans.append(f"{start}->{nums[i-1]}")
-                else: ans.append(f"{nums[i-1]}")
+                if start != nums[i-1]: 
+                    ans.append(f"{start}->{nums[i-1]}")
+                else: 
+                    ans.append(f"{nums[i-1]}")
                 #ans.append(f"{start}->{nums[i-1]}" if start != nums[i-1] else f"{nums[i-1]}")
                 start = nums[i]
         
-        
+        if start == nums[-1]: 
+            ans.append(f"{start}")
+        else: 
+            ans.append(f"{start}->{nums[-1]}")
+
         return ans
