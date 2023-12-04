62801015b31f0634948157c1e96e470041f340b1
OhJeong-Taek <ojt0128@gmail.com>
2023-12-04 22:51:54 +0900

    Time: 61 ms (54.82%) | Memory: 16.2 MB (51.54%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -3,7 +3,7 @@ class Solution:
         if x < 0: return False
         s = str(x)
         for i in range(len(s)//2):
-            if s[i] != s[len(s)-1-i]:
+            if s[i] != s[-1-i]:
                 return False
         return True
             
